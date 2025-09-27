09cf56ea1a402ef2045e84e5363ae7817085517a
fuji <info@fuji-soho.net>
2025-09-27 17:26:45 +0900

    スマホのプライバシーボタン修正

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?17">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?18">
 
 
 <script>
